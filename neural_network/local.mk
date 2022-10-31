fe5272eb2ac65d85d1828d06caf08d2cd91a6af2
Mirko Mariotti <mirko@mirkomariotti.it>
2022-10-31 13:31:10 +0100

    Changes

--- neural_network/local.mk
+++ neural_network/local.mk
@@ -1,16 +1,19 @@
 WORKING_DIR=working_dir
 CURRENT_DIR=$(shell pwd)
-SOURCE_NEURALBOND=banknote.json
+SOURCE_NEURALBOND=<<PLACEHOLDER FILE>>.json
 NEURALBOND_LIBRARY=neurons
-BOARD=ebaz4205
-MAPFILE=ebaz4205_maps.json
+NEURALBOND_ARGS=-config-file neuralbondconfig.json -operating-mode fragment
+BMINFO=bminfo.json
+BOARD=<<PLACEHOLDER BOARD>>
+MAPFILE=<<PLACEHOLDER BOARD>>_maps.json
 SHOWARGS=-dot-detail 5
 SHOWRENDERER=dot
 VERILOG_OPTIONS=-comment-verilog
 DBASM_ARGS=-d
-BENCHCORE=i0,p22o0
+#BENCHCORE=i0,p22o0
 #HDL_REGRESSION=bondmachine.sv
 #BM_REGRESSION=bondmachine.json
 include bmapi.mk
 include crosscompile.mk
 include buildroot.mk
+include simbatch.mk
