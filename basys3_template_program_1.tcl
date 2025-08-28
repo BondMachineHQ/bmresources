6def027bdfb4e139140e71e56e75ad3cf88cd362
Mirko Mariotti <mirko@mirkomariotti.it>
2025-08-28 11:49:28 +0200

    :sparkles: peerid based program for basys3

--- basys3_template_program_1.tcl
+++ basys3_template_program_1.tcl
@@ -1,4 +1,4 @@
 open_hw
 connect_hw_server
 open_hw_target
-set_property PROBES.FILE {} [lindex [get_hw_devices] 0]
+set_property PROBES.FILE {} [lindex [get_hw_devices] --PEERID--]
--- basys3_template_program_2.tcl
+++ basys3_template_program_2.tcl
@@ -1,3 +1,3 @@
-program_hw_devices [lindex [get_hw_devices] 0]
-refresh_hw_device [lindex [get_hw_devices] 0]
+program_hw_devices [lindex [get_hw_devices] --PEERID--]
+refresh_hw_device [lindex [get_hw_devices] --PEERID--]
 exit
